969dfdf7daaa8acc5ef60257f89fa04337886a86
abelardom3 <abelardom1999@gmail.com>
2022-09-13 15:25:26 -0400

    second commit

--- queues/queues.js
+++ queues/queues.js
@@ -10,7 +10,6 @@ class Node {
         this.data = data
         this.next = null
     }
-
 }
 
 
@@ -26,11 +25,54 @@ class Queue {
 
 
 
+    enqueue(data) {
+        const node = new Node(data)
+        if (!this.first) {
+            this.first = node
+            this.last = node
+
+        } else {
+            this.last.next = node
+            this.last = node
+        }
+
+        this.length++
+        return this
+    }
 
 
 
+
+    dequeue() {
+        const trackingNode = this.first
+
+        if (this.first === this.last) {
+            this.first = null
+            this.last = null
+        }
+        else {
+            this.first = this.first.next
+        }
+        this.length--
+        return trackingNode
+    }
 
 
 
 }
+
+const q = new Queue()
+
+
+q.enqueue('1st person')
+q.enqueue('2nd person')
+q.enqueue('3rd person')
+q.enqueue('4th person')
+// q.dequeue()
+// q.dequeue()
+// q.dequeue()
+// q.dequeue()
+
+console.log(q)
+
 
